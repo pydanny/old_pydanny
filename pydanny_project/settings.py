8ca99f827aef3c0ed06a289d0c47b4f6a0fd6019
pydanny <pydanny@gmail.com>
2010-09-02 20:37:17 -0500

    Working on the core project

---
+++ pydanny_project/settings.py
@@ -0,0 +1,107 @@
+# Django settings for pydanny_project project.
+
+import os.path
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SERVE_MEDIA = DEBUG
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
+ADMINS = (
+    ("pydanny", "pydanny@gmail.com"),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "dev.db",                      # Or path to database file if using sqlite3.
+        "USER": "",                      # Not used with sqlite3.
+        "PASSWORD": "",                  # Not used with sqlite3.
+        "HOST": "",                      # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "",                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = "America/Chicago"
+
+LANGUAGE_CODE = "en-us"
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
+
+MEDIA_URL = "/site_media/media/"
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
+
+STATIC_URL = "/site_media/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, "media"),
+]
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = "/media/"
+
+# Make this unique, and don"t share it with anybody.
+SECRET_KEY = "fj4-3w&mf_dzdkstk!-*!x3p=ce@nd@i*myvk6tz@r&5cw(=4%"
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+#     "django.template.loaders.eggs.Loader",
+)
+
+MIDDLEWARE_CLASSES = (
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.flatpages.middleware.FlatpageFallbackMiddleware",    
+)
+
+ROOT_URLCONF = "pydanny_project.urls"
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, "templates"),
+)
+
+INSTALLED_APPS = (
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.flatpages",    
+    "django.contrib.humanize",    
+    "django.contrib.messages",    
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "blogger"
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+
+    "staticfiles.context_processors.static_url",
+
+]
